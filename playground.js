85e71999ce3c60d9725d788114d50b53f30e9af8
Yuji Ikebata <yjikbt@gmail.com>
2015-09-17 21:23:32 +0900

    modified logic in onOpen()

--- playground.js
+++ playground.js
@@ -1,32 +1,4 @@
 function onOpen() {
-	// get today's date
-	var sheet = SpreadsheetApp.getActiveSheet();
-	var data = sheet.getRange(2, 1);
-	Logger.log(data);
-
-	// check if a moment is before another moment
-
-	// generate new sheet
-	// var moment = Moment.moment();
-
-	// moment.locale('ja', {
-	// 	weekdays: ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"],
-	// 	weekdaysShort: ["日","月","火","水","木","金","土"]
-	// });
-
-	// var today = moment.format("YYYY[/]MM[/]DD[（]ddd[）]");
-
-	// var yesterday = moment.subtract(1, 'days'));
-
-	// if (moment.isSame(yesterday) {
-	// 	Browser.msgBox(yesterday);
-	// }
-	// var date = moment.date();
-	// var month = moment.month() + 1;
-	// var year = moment.year();
-}
-
-function generateNewSheet() {
 	// get today's date from a active spreadsheet
 	var sheet = SpreadsheetApp.getActiveSheet();
 	var cell = sheet.getRange(2, 1);
@@ -35,7 +7,10 @@ function generateNewSheet() {
 	// generate moment objects
 	var today = Moment.moment();
 	var yesterday = Moment.moment(values);
+	generateNewSheet(sheet, today, yesterday);
+}
 
+function generateNewSheet(sheet, today, yesterday) {
 	// compare today's day with yesterday's
 	if (today.isAfter(yesterday, 'day')) {
 		// insert a new row
